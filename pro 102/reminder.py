1f24ca943844f9340f95ebb13ad74bfec1cdcc73
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2021-06-03 17:22:26 +0530

    Update reminder.py
    added list and remove space from  split()

--- pro 102/reminder.py
+++ pro 102/reminder.py
@@ -3,11 +3,13 @@ import time
 start= time.time()
 #reminderObj = [str(r) for r in input("Enter the things you want to be reminded:").split(', ')]
 #print("\n",r)
-reminderObj=input("Enter the things you want to be reminded seprated by a comma:").split(', ')
+reminderObj=input("Enter the things you want to be reminded seprated by a comma:").split(',')
 
+reminderList = []
 for r in reminderObj:
     remind=r
+    reminderList.append(remind)
 
 if ((time.time()-start)>=2):
-    print("the things you need to do are:",remind)
+    print("the things you need to do are:",reminderList)
     reminderObj=input("If you have to change the reminder then type here:").split(",")

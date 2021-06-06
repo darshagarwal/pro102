68c690c41722d727054714cfcf99fadb5110da1f
darshagarwal <64535202+darshagarwal@users.noreply.github.com>
2021-06-06 17:30:39 +0530

    Add files via upload

--- pro 102/reminder.py
+++ pro 102/reminder.py
@@ -4,12 +4,14 @@ start= time.time()
 #reminderObj = [str(r) for r in input("Enter the things you want to be reminded:").split(', ')]
 #print("\n",r)
 reminderObj=input("Enter the things you want to be reminded seprated by a comma:").split(',')
+time_to_repete=input("Enter the time you want the timer to repeat:")
 
 reminderList = []
 for r in reminderObj:
     remind=r
     reminderList.append(remind)
 
-if ((time.time()-start)>=2):
-    print("the things you need to do are:",reminderList)
+if ((time.time()-start)>=3600):
+    print("the things you need to do are:",*reminderList,sep = "\n")
     reminderObj=input("If you have to change the reminder then type here:").split(",")
+    time_to_repete=input("Enter the time you want the timer to repeat:")
